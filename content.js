3f046c1946d22f50075a0fa5ebd08efaf3ca84b8
Brian <yan.briann@gmail.com>
2023-12-20 19:45:52 -0500

    added teams

--- content.js
+++ content.js
@@ -1,1 +1,23 @@
+const currentUrl = window.location.href;
+const obj = {'cleveland-cavaliers': 'red'}
 
+for (let key in obj){
+  if (currentUrl.indexOf(key) > -1){
+    document.querySelector('body').style.color = obj[key]
+  }
+}
+currentUrl.indexOf('cleveland-cavaliers')
+
+
+
+const teamObj = {
+    'boston-celtics': green ,
+    'brooklyn-nets' : black ,
+    'new-york-knicks' : orange,
+    'philadelphia-76ers'
+    toronto-raptors
+    chicago-bulls
+    cleveland-cavaliers
+
+
+}
