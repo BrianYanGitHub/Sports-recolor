12cd57d3ba403081cd17c7ddd298523fc35d3e86
Brian <yan.briann@gmail.com>
2023-12-20 19:59:08 -0500

    added color obj

--- content.js
+++ content.js
@@ -1,23 +1,45 @@
 const currentUrl = window.location.href;
-const obj = {'cleveland-cavaliers': 'red'}
+// const obj = {'cleveland-cavaliers': 'red'}
+const teamObj = {
+  'boston-celtics': green ,
+  'brooklyn-nets' : black ,
+  'new-york-knicks' : orange,
+  'philadelphia-76ers' : blue,
+  'toronto-raptors' : red,
+  'chicago-bulls' : red,
+  'cleveland-cavaliers' : red,
+  'detroit-pistons': blue,
+  'indiana-pacers':yellow,
+  'milwaukee-bucks': green,
+  'atlanta-hawks': red,
+  'charlotte-hornets': purple,
+  'miami-heat': red,
+  'orlando-magic': lightblue,
+  'washington-wizards': navy,
+  'denver-nuggets': blue,
+  'minnesota-timberwolves': navy,
+  'oklahoma-city-thunder': aqua,
+  'portland-trail-blazers': red,
+  'utah-jazz': blue,
+  'golden-state-warriors': yellow,
+  'la-clippers': black,
+  'los-angeles-lakers': gold,
+  'phoenix-suns': orange,
+  'sacramento-kings': purple,
+  'dallas-mavericks': blue,
+  'houston-rockets': red,
+  'memphis-grizzlies': lightblue,
+  'new-orleans-pelicans': navy,
+  'san-antonio-spurs': grey,
+}
+
 
 for (let key in obj){
   if (currentUrl.indexOf(key) > -1){
     document.querySelector('body').style.color = obj[key]
   }
 }
-currentUrl.indexOf('cleveland-cavaliers')
 
 
 
-const teamObj = {
-    'boston-celtics': green ,
-    'brooklyn-nets' : black ,
-    'new-york-knicks' : orange,
-    'philadelphia-76ers'
-    toronto-raptors
-    chicago-bulls
-    cleveland-cavaliers
 
-
-}
