3eff34c80b2f0bebbd41a4372e88f333c6e40b5d
Brian <yan.briann@gmail.com>
2023-12-20 20:04:23 -0500

    8:04pm

--- content.js
+++ content.js
@@ -1,42 +1,42 @@
 const currentUrl = window.location.href;
 // const obj = {'cleveland-cavaliers': 'red'}
 const teamObj = {
-  'boston-celtics': green ,
-  'brooklyn-nets' : black ,
-  'new-york-knicks' : orange,
-  'philadelphia-76ers' : blue,
-  'toronto-raptors' : red,
-  'chicago-bulls' : red,
-  'cleveland-cavaliers' : red,
-  'detroit-pistons': blue,
-  'indiana-pacers':yellow,
-  'milwaukee-bucks': green,
-  'atlanta-hawks': red,
-  'charlotte-hornets': purple,
-  'miami-heat': red,
-  'orlando-magic': lightblue,
-  'washington-wizards': navy,
-  'denver-nuggets': blue,
-  'minnesota-timberwolves': navy,
-  'oklahoma-city-thunder': aqua,
-  'portland-trail-blazers': red,
-  'utah-jazz': blue,
-  'golden-state-warriors': yellow,
-  'la-clippers': black,
-  'los-angeles-lakers': gold,
-  'phoenix-suns': orange,
-  'sacramento-kings': purple,
-  'dallas-mavericks': blue,
-  'houston-rockets': red,
-  'memphis-grizzlies': lightblue,
-  'new-orleans-pelicans': navy,
-  'san-antonio-spurs': grey,
+  'boston-celtics': 'green' ,
+  'brooklyn-nets' : 'black' ,
+  'new-york-knicks' : 'orange',
+  'philadelphia-76ers' : 'blue',
+  'toronto-raptors' : 'red',
+  'chicago-bulls' : 'red',
+  'cleveland-cavaliers' : 'brown',
+  'detroit-pistons': 'blue',
+  'indiana-pacers':'yellow',
+  'milwaukee-bucks': 'green',
+  'atlanta-hawks': 'red',
+  'charlotte-hornets': 'purple',
+  'miami-heat': 'red',
+  'orlando-magic': 'lightblue',
+  'washington-wizards': 'navy',
+  'denver-nuggets': 'blue',
+  'minnesota-timberwolves': 'navy',
+  'oklahoma-city-thunder': 'aqua',
+  'portland-trail-blazers': 'red',
+  'utah-jazz': 'blue',
+  'golden-state-warriors': 'yellow',
+  'la-clippers': 'black',
+  'los-angeles-lakers': 'gold',
+  'phoenix-suns': 'orange',
+  'sacramento-kings': 'purple',
+  'dallas-mavericks': 'blue',
+  'houston-rockets': 'red',
+  'memphis-grizzlies': 'lightblue',
+  'new-orleans-pelicans': 'navy',
+  'san-antonio-spurs': 'grey'
 }
 
 
-for (let key in obj){
+for (let key in teamObj){
   if (currentUrl.indexOf(key) > -1){
-    document.querySelector('body').style.color = obj[key]
+    document.querySelector('body').style.color = teamObj[key]
   }
 }
 
